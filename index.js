6a53b205b91d6a8a3af94b4aba7ee032205cdc05
Fahad Haidari <fahad.haidari@gmail.com>
2019-02-06 20:44:46 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+  const buffer = document.createElement('canvas');
+  const input = document.createElement('input');
+  const context = buffer.getContext('2d');
+  const WIDTH = buffer.width = 400;
+  const HEIGHT = buffer.height = 400;
+  const DEFAULT_COLOR = '#EEEEEE';
+  let color = DEFAULT_COLOR;
+
+  document.body.appendChild(buffer);
+  document.body.appendChild(input);
+
+  input.placeholder = 'Enter color...'
+
+  const fillColor = function() {
+    context.fillStyle = color;
+    context.fillRect(0, 0, WIDTH, HEIGHT);
+  };
+
+  input.addEventListener('input', (e) => {
+      color = e.target.value;
+      context.fillStyle = '#' + color;
+
+      if (!color) color = DEFAULT_COLOR;
+      fillColor(color);
+  });
+
+  input.focus();
+  input.placeholder = 'Enter color...'
+  fillColor();
+};
